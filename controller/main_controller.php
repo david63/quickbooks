4ce0e87c4e62f038b1c3358228bd230d970c1926
david63 <david.wood@pear-tree.org.uk>
2020-08-19 15:33:26 +0100

    Initial commit

---
+++ controller/main_controller.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package Quick Link Bookmarks Extension
+* @copyright (c) 2020 david63
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace david63\quickbooks\controller;
+
+/**
+* Main controller
+*/
+class main_controller
+{
+	/** @var string phpBB root path */
+	protected $root_path;
+
+	/** @var string phpBB extension */
+	protected $php_ext;
+
+	/**
+	* Constructor for main controller
+	*
+	* @param string									$root_path			phpBB root path
+	* @param string									$php_ext			phpBB extension
+	*
+	* @return \phpbb\quickbooks\controller\main_controller
+	* @access public
+	*/
+	public function __construct(string $root_path, string $php_ext)
+	{
+		$this->root_path	= $root_path;
+		$this->php_ext		= $php_ext;
+	}
+
+	/**
+	* Display the options a user can configure for this extension
+	*
+	* @return null
+	* @access public
+	*/
+	public function quick_books()
+	{
+		return redirect(append_sid($this->root_path . 'ucp.' . $this->php_ext . '?i=ucp_main&mode=bookmarks'));
+	}
+}
